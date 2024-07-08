d815941890bb5dc2a86bc146f10aedf44733c5ed
Marco Piolo Mercado <marcopiolo.mercado@bisu.edu.ph>
2024-07-08 10:51:50 +0800

    implement_pagination
    "Updated ProductController to add pagination and error handling for index and search methods"

---
+++ app/Http/Resources/ProductResourceCollection.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductResourceCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+
+        return [
+            'data' => $this->collection->map(function ($item) {
+                return new ProductResource($item);
+            }),
+            'links' => [
+                'self' => url()->current(),
+                'first' => $this->url(1),
+                'last' => $this->url($this->lastPage()),
+                'prev' => $this->previousPageUrl(),
+                'next' => $this->nextPageUrl(),
+            ],
+            'meta' => [
+                'current_page' => $this->currentPage(),
+                'from' => $this->firstItem(),
+                'last_page' => $this->lastPage(),
+                'path' => url()->current(),
+                'per_page' => $this->perPage(),
+                'to' => $this->lastItem(),
+                'total' => $this->total(),
+            ],
+        ];
+    }
+}
