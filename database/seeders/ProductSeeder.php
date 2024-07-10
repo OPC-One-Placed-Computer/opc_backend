e618bfa48bdd0fea7bfcbf6def5636c08c3385db
Marco Piolo Mercado <marcopiolo.mercado@bisu.edu.ph>
2024-07-10 17:33:22 +0800

    products_seeder
    "Updated ProductController to change pagination limit, modified ProductFactory to generate image files and updated product attributes, and updated DatabaseSeeder to call ProductSeeder."

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,11 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Product;
 
-class ProductsSeeder extends Seeder
+class ProductSeeder extends Seeder
 {
     /**
      * Run the database seeds.
