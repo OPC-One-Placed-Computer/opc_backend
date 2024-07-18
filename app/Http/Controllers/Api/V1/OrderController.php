0d42996bb10079f2df3ada6005a18e89b005e864
Marco Piolo Mercado <marcopiolo.mercado@bisu.edu.ph>
2024-07-18 14:44:30 +0800

    implement_stripe
    "Added Laravel Cashier package, updated OrderController to remove placeOrder method, added PaymentController, updated routes/api.php to include new payment routes, and made various other code changes."

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -5,9 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\BaseController;
 use App\Http\Resources\OrderResource;
 use App\Http\Resources\OrderResourceCollection;
-use App\Models\CartItem;
 use App\Models\Order;
-use App\Models\OrderItem;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Exception;
@@ -17,6 +15,7 @@ class OrderController extends BaseController
 {
     public function index(Request $request)
     {
+
         try {
             $query = Order::where('user_id', auth()->user()->id)
                 ->with('orderItems.product');
@@ -51,64 +50,6 @@ class OrderController extends BaseController
             return $this->sendResponse('Orders fetched successfully', $orderCollection);
         } catch (Exception $exception) {
             return $this->sendError('Failed to fetch orders', [], 500);
-        }
-    }
-
-    public function placeOrder(Request $request)
-    {
-
-        $validatedData = $request->validate([
-            'full_name' => 'required|string',
-            'shipping_address' => 'required|string',
-            'total' => 'required|numeric|min:0',
-            'cart_items' => 'required|array',
-            'cart_items.*.id' => 'required|exists:cart_items,id,user_id,' . auth()->user()->id,
-        ]);
-
-        $total = $validatedData['total'];
-        $cartItemIds = collect($validatedData['cart_items'])->pluck('id');
-        $cartItems = CartItem::whereIn('id', $cartItemIds)->where('user_id', auth()->user()->id)->get();
-
-        if ($cartItems->isEmpty()) {
-            return $this->sendError('Selected cart items not found or do not belong to the user', [], 400);
-        }
-
-        DB::beginTransaction();
-        try {
-            // Create order
-            $order = Order::create([
-                'user_id' => auth()->user()->id,
-                'full_name' => $validatedData['full_name'],
-                'shipping_address' => $validatedData['shipping_address'],
-                'total' => $total,
-                'status' => 'pending', // Default status
-            ]);
-
-            // Create order items for selected cart items
-            foreach ($cartItems as $cartItem) {
-                $product = $cartItem->product;
-
-                // Ensure the product exists and fetch the price
-                if ($product) {
-                    OrderItem::create([
-                        'order_id' => $order->id,
-                        'product_id' => $cartItem->product_id,
-                        'quantity' => $cartItem->quantity,
-                        'subtotal' => $product->price * $cartItem->quantity,
-                    ]);
-                }
-
-                $cartItem->delete();
-            }
-
-            // Eager load order items relationship with product details
-            $order->load('orderItems.product');
-
-            DB::commit();
-            return $this->sendResponse('Order placed successfully', new OrderResource($order));
-        } catch (Exception $exception) {
-            DB::rollBack();
-            return $this->sendError($exception->getMessage());
         }
     }
 
