18308d845fc30e3219754c1afea046948ec2a2b0
Marco Piolo Mercado <marcopiolo.mercado@bisu.edu.ph>
2024-07-22 20:53:49 +0800

    modify_checkout_success
    "Refactored OrderController, added show method, removed cancelledOrders and allOrders methods, updated PaymentController, modified routes in api.php and web.php, and updated package-lock.json"

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\api\V1\AuthController;
+use App\Http\Controllers\Api\V1\CheckoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,6 @@ Route::get('/', function () {
 
 Route::post("/register", [AuthController::class, "register"]);
 Route::post("/login", [AuthController::class, "login"]);
+
+Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+Route::get('checkout/cancel', [CheckoutController::class, 'cancel'])->name('checkout.cancel');
